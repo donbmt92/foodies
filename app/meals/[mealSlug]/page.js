e4bd40cfa53da0f97370c1b942f8d016d84148ae
donbmt92 <71434503+donbmt92@users.noreply.github.com>
2024-04-08 16:49:13 +0700

    ComponentSetup

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,9 @@
+export default function MealDetailShare() {
+    return (
+      <main>
+        <h1 style={{ color: 'white', textAlign: 'center' }}>
+        MealDetail
+        </h1>
+      </main>
+    );
+  }
