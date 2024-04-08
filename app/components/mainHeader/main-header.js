ec2bf46c1f33d6aeb854e4f736c989a77ce1a8a7
donbmt92 <71434503+donbmt92@users.noreply.github.com>
2024-04-08 22:02:55 +0700

    customHeader

---
+++ app/components/mainHeader/main-header.js
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+import logoImg from '@/assets/logo.png';
+import classes from './main-header.module.css';
+import Image from 'next/image';
+import MainHeaderBackGround from './main-header-background';
+
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackGround />
+      <header className={classes.header}>
+        <Link href="/" className={classes.logo}>
+          <Image
+            src={logoImg}
+            className={classes.logo}
+            alt="A plate with food on it"
+            priority
+          />
+          Next Level Food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foody Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
