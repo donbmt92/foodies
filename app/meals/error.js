10fdf96839fcaa17927af629e98a295d6588e6ee
donbmt92 <71434503+donbmt92@users.noreply.github.com>
2024-04-10 16:02:38 +0700

    HandlingError

---
+++ app/meals/error.js
@@ -0,0 +1,8 @@
+'use client'
+
+export default function Error({error}) {
+  return <main className="error">
+    <h1>An Error occured</h1>
+    <p>Failed to featch meal data. Please try again later.</p>
+  </main>
+}
