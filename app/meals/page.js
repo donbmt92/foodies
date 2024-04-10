b7f63caaa27104eaa8d7e8de36046b313a07eb04
donbmt92 <71434503+donbmt92@users.noreply.github.com>
2024-04-10 15:15:52 +0700

    Fetching Data

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,7 +2,10 @@ import MealsGrid from '../components/meals/meals-grid';
 import classes from './page.module.css';
 import Link from 'next/link';
 import '../globals.css';
-export default function MealsPage() {
+import { getMeals } from '@/lib/meals';
+export default async function MealsPage() {
+  const meals = await getMeals();
+  
   return (
     <>
       <header className={classes.header}>
@@ -17,7 +20,7 @@ export default function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]}/>
+        <MealsGrid meals={meals}/>
       </main>
     </>
   );
