d2b2b6574988976e4235ae2a3147cb3e6793ca74
donbmt92 <71434503+donbmt92@users.noreply.github.com>
2024-04-10 18:55:08 +0700

    SharePage + PickedImage Review

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,4 +1,6 @@
 import classes from './page.module.css';
+import '../../globals.css'
+import ImagePicker from '@/app/components/images/image-picker';
 
 export default function ShareMealPage() {
   return (
@@ -38,7 +40,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          IMAGE PICKER
+          <ImagePicker/>
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
