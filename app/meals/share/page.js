ef0f8018c0d6cdd1c9328c13643b2c5fe271c70c
donbmt92 <71434503+donbmt92@users.noreply.github.com>
2024-04-11 15:06:40 +0700

    addDatavlueToDB

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,8 +1,10 @@
 import classes from './page.module.css';
-import '../../globals.css'
+import '../../globals.css';
 import ImagePicker from '@/app/components/images/image-picker';
+import { shareMeal } from '@/lib/actions';
 
 export default function ShareMealPage() {
+  
   return (
     <>
       <header className={classes.header}>
@@ -12,7 +14,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -40,7 +42,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker/>
+          <ImagePicker label='Your Image' name='image'/>
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
